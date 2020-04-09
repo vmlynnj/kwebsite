6189db512bd80bcbf79f697267854b8f1d83847d
vmlynnj <vmlynnj@gmail.com>
2020-04-09 16:09:20 -0400

    added nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
+import { PublicationsComponent } from './publications/publications.component';
+import { AccoladesComponent } from './accolades/accolades.component';
+import { ReadComponent } from './read/read.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'publications', component: PublicationsComponent},
+  { path: 'accolades', component: AccoladesComponent},
+  { path: 'read', component: ReadComponent},
+  { path: 'contact', component: ContactComponent},
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { PublicationsComponent } from './publications/publications.component';
+import { AccoladesComponent } from './accolades/accolades.component';
+import { ReadComponent } from './read/read.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    PublicationsComponent,
+    AccoladesComponent,
+    ReadComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
